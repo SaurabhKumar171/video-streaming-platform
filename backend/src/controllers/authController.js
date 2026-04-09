c84faea903d4398d898b030480878a589044fe20
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2026-04-09 17:05:42 +0530

    fix: updated cookies

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -16,13 +16,14 @@ exports.login = async (req, res) => {
   try {
     const { username, password } = req.body;
     const { token, user } = await authService.loginUser(username, password);
+    const isProduction = process.env.NODE_ENV === "production";
 
     // Set Cookie Options
     const cookieOptions = {
       expires: new Date(Date.now() + 24 * 60 * 60 * 1000), // 1 day
       httpOnly: true, // Prevents JS access (Security!)
-      secure: false, // Set to false for localhost/HTTP
-      sameSite: "lax", // Required for cross-origin cookies in modern browsers
+      secure: isProduction, // true in prod, false in local
+      sameSite: isProduction ? "None" : "Lax",
       // path: "/",
     };
 
